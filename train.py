73f77f99a4c813b21d02f0ca9e140aaacdf541a2
shlomi amitai <shlomi@shopic.co>
2025-02-04 13:45:45 +0200

    relative path

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ import math
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--data_dir', type=str, default='/home/shlomia/work/myrepo/Visual-Selective-VIO/data', help='path to the dataset')
+parser.add_argument('--data_dir', type=str, default='./data', help='path to the dataset')
 parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
 parser.add_argument('--save_dir', type=str, default='./results', help='path to save the result')
 
